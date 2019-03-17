b2776365a75197a83e996b631ff2a64ad2d6b75b
Amulya Reddy <amulyareddyk@yahoo.in>
2019-03-18 00:43:20 +0530

    Delete Ex3_1.R

--- Ex3_1.R
+++
@@ -1,9 +0,0 @@
-# page number: 75
-
-#meals
-appetizers<-c("soup","juice")
-main<-c("meat", "fish", "vegetable dish")
-desert<-c("ice cream","cake")
-
-ans<-length(appetizers)*length(main)*length(desert)
-print(ans)
