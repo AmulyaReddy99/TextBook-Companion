79f372df9ecd2b622dfcbf07e7e405d43118346e
AmulyaReddy99 <amulyareddyk@yahoo.in>
2019-03-30 15:08:28 +0530

    commit on more files

--- CH 11/Ex11_4.R
+++ CH 11/Ex11_4.R
@@ -1,5 +1,8 @@
 # page number: 409
 
+yes <- c('1-a', 'a')
+no <- c('b', '1-b')
 
+p <- data.frame(yes,no)
 
-
+print(p)
---
+++ CH 11/Ex11_5.R
@@ -0,0 +1,9 @@
+# page number: 409
+
+W <- c(.5, .25, .25)
+P <- c(.5, .25, .25)
+S <- c(.5, .25, .25)
+
+p <- data.frame(W, P, S)
+
+print(p)
