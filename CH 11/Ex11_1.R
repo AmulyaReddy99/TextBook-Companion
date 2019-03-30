47bab0d0105e01677c5e838898df092561fda6cd
AmulyaReddy99 <amulyareddyk@yahoo.in>
2019-03-30 14:09:38 +0530

    ch 11 first sum

--- CH 11/Ex11_1.R
+++ CH 11/Ex11_1.R
@@ -1,3 +1,14 @@
 # page number: 406
 
+library(MASS)
 
+X <- c("R", "N", "S")
+
+R <- c(1/2, 1/4, 1/4)
+N <- c(1/2, 0, 1/2)
+S <- c(1/4, 1/4, 1/2)
+
+P<-data.frame(R, N, S)
+
+P <- rbind(R, N, S)
+print(fractions(P))
